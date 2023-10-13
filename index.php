11533341d28d809346154dcf03e40122a4856003
SergeyRushev <rushevs198417@gmail.com>
2023-10-13 08:47:56 +0300

    feat: add input of the number of coins from the user

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 
 require_once './vendor/autoload.php';
 
+echo "Enter the number of coins for Joe: ";
+$coins1 = fgets(STDIN);
+
+echo "Enter the number of coins for Jane: ";
+$coins2 = fgets(STDIN);
+
+
 $game = new Game(
-    new Player("Joe", 10000),
-    new Player("Jane", 100)
+    new Player("Joe", $coins1),
+    new Player("Jane", $coins2)
 );
 
 $game->start();
